7abb04126e4d17e05e07e7ae8a2572feebd0a4b5
Sam D <samidiaz3@gmail.com>
2021-02-05 03:20:51 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -49,11 +49,11 @@ end
 
 # 9. adds an "s" to each word in array EXCEPT the second element
 def add_s(array)
-  array.each_with_index.collect { |element, index|
+  array.each_with_index.collect {|element, index|
     if index == 1
       element
     else
       element + "s"
-    end  
+    end
   }
 end
