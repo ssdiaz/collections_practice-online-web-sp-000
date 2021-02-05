49f4d4cef74f6e036cbf119fad4f8b5424c0de8d
Sam D <samidiaz3@gmail.com>
2021-02-05 02:28:52 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,26 +1,36 @@
-# returns a copy in ascending order
+# 1. returns a copy in ascending order
 def sort_array_asc (array)
   array.sort
 end
 
-# returns desending order
+# 2. returns desending order
 def sort_array_desc(array)
   array.sort! { |x,y| y <=> x }
 end
 
-# returns a copy of array with the strings in asc. order by length
+# 3. returns a copy of array with the strings in asc. order by length
 def sort_array_char_count(array)
   array.sort do |a,b|
     a.length <=> b.length
   end
 end
 
-# swaps the second and third elements
+# 4. swaps the second and third elements
 def swap_elements(array)
-
+  array [1], array [2] =  array [2],  array [1]
+  array
 end
 
-
+# 5. returns a copy of array reversed
 def reverse_array(array)
   array.reverse
 end
+
+# 6. replaces the thrid character to $
+def kesha_maker (array)
+
+end
+
+# 7.
+# 8.
+# 9.
