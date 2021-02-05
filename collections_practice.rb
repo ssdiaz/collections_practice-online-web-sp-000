17ae19b37ac7db7adeeaf06deda4c9428173bed4
Sam D <samidiaz3@gmail.com>
2021-02-05 01:56:32 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -5,12 +5,9 @@ end
 
 # returns desending order
 def sort_array_desc(array)
-  array.sort! { |x,y| y <=> x }
-end
-
-# returns a copy of array with the strings in asc. order by length
-def sort_aray_char_count
   array.sort do |a,b|
     a.length <=> b.length
   end
 end
+
+# returns a copy of array with the strings in asc. order by length
