d9bfe1e303e7a509f4781e9ad9c87f44efa70439
Sam D <samidiaz3@gmail.com>
2021-02-05 03:18:24 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -51,8 +51,8 @@ end
 def add_s(array)
   array.each_with_index.collect do |element, index|
     if index == 1
-      element 
-    else 
+      element
+    else
       element + "s"
     end
   end
